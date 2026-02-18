15f86aafdd73b6daec0a415edab8b45b5c21c7bf
IgorS11021988 <starostinigo@yandex.ru>
2026-02-18 16:32:16 +0300

    Отображаем графики в консоли

--- MaterialDegradation/InputOutput.py
+++ MaterialDegradation/InputOutput.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib.pyplot import show
 
 from MathProtEnergyProcSynDatas.TimesMoments import LinearTimesMoments
 from MathProtEnergyProcSynDatas.Indicate import PlotGraphicIndicate, SaveDynamicToFileIndicate
@@ -131,3 +132,4 @@ def OutputValues(dyns, fileName,
                                saveGraphicIndicator=PlotGraphicIndicate,  # Индикатор отображения графиков
                                index=index  # Индекс динамики
                                )
+  show()
